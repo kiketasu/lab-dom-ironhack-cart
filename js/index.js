d81e1a5f366a986f84ec44681da6835d1fe314c2
Enric Orillosa <enricorillosa@hotmail.es>
2022-09-27 17:15:13 +0200

    iteration 3
    total updated

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
   //... your code goes here
   let priceParent = product.querySelector('.price span');
+  
   let price = priceParent.innerHTML;
-  let quantity = product.querySelector('.quantity input')
-  let subtotal = product.querySelector('.subtotal')
-  subtotal.innerHTML = `$<span>${quantity.value * price}</span>`
+
+  let quantity = product.querySelector('.quantity input');
+  let subtotal = product.querySelector('.subtotal');
+  subtotal.innerHTML = `$<span>${quantity.value * price}</span>`;
 };
 
 function calculateAll() {
@@ -17,15 +18,23 @@ function calculateAll() {
   let products = document.getElementsByClassName('product');
   for (let i = 0; i < products.length; i++) {
     const element = products[i];
-    updateSubtotal(element)
-    
-  }
-  
-
+    updateSubtotal(element);
+  };
   // ITERATION 3
   //... your code goes here
-}
-
+  let subtotalAll = document.querySelectorAll('.subtotal span');
+  let total = document.querySelector('#total-value')
+  let sum = 0;
+  for (let i = 0; i < subtotalAll.length; i++) {
+    const element = subtotalAll[i].innerHTML;
+    console.log(element);
+    //para 
+    sum += parseFloat(element);
+ };
+ console.log(sum)
+  total.innerHTML = `Total: $<span> ${sum}</span>`
+  
+};
 // ITERATION 4
 
 function removeProduct(event) {
